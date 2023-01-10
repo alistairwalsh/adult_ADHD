e58163f5863945fa95efd70ce6d662fbd0f958f0
Alistair Walsh <alistair.walsh@me.com>
2023-01-10 15:07:33 +1100

    horizontal

--- pages/Symptom_Checklist.py
+++ pages/Symptom_Checklist.py
@@ -45,7 +45,7 @@ text = [
 'How often do you interrupt others when they are busy?']
 
 for question in text:
-    st.radio(question, options)
+    st.radio(question, options, horizontal = True)
 
 #text = [part for part in text.split('\n') if part.strip() not in ['Page 1 of 2', 'NovoPsych','PART A -','PART B -']]
 
