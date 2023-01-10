cfe713055016d2c58c2f432ab455398daacbecde
Alistair Walsh <alistair.walsh@me.com>
2023-01-10 14:43:49 +1100

    strip

--- pages/Symptom_Checklist.py
+++ pages/Symptom_Checklist.py
@@ -92,7 +92,7 @@ required?
 when they are busy? '''
 
 
-text = [part for part in text.split('\n') if part not in ['Page 1 of 2', 'NovoPsych']]
+text = [part for part in text.split('\n') if part.strip() not in ['Page 1 of 2', 'NovoPsych']]
 
 for line in text:
     st.text(line)
