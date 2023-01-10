425a86d540b39aeca6bcec1a6bdd9300db711d70
Alistair Walsh <alistair.walsh@me.com>
2023-01-10 15:03:00 +1100

    split questions

--- pages/Symptom_Checklist.py
+++ pages/Symptom_Checklist.py
@@ -7,76 +7,46 @@ yourself over the past 6 months.''')
 
 options = ['Never','Rarely','Sometimes','Often','Very Often']
 
-text = '''
-How often do you have trouble wrapping up the final details
-of a project, once the challenging parts have been done?
+text = [
+'How often do you have trouble wrapping up the final details of a project, once the challenging parts have been done?',
 
-How often do you have difficulty getting
-things in order when you have to do a
-task that requires organisation?
+'How often do you have difficulty getting things in order when you have to do a task that requires organisation?',
 
-How often do you have problems
-remembering appointments or
-obligations?
+'How often do you have problems remembering appointments or obligations?',
 
-When you have a task that requires a lot
-of thought, how often do you avoid or
-delay getting started?
+'When you have a task that requires a lot of thought, how often do you avoid or delay getting started?',
 
-How often do you fidget or squirm with
-your hands or feet when you have to sit
-down for a long time?
+'How often do you fidget or squirm with your hands or feet when you have to sit down for a long time?',
 
-How often do you feel overly active and
-compelled to do things, like you were
-driven by a motor?
+'How often do you feel overly active and compelled to do things, like you were driven by a motor?',
 
-How often do you make careless mistakes when you
-have to work on a boring or difficult project?
+'How often do you make careless mistakes when you have to work on a boring or difficult project?',
 
-How often do you have difficulty keeping
-your attention when you are doing boring
-or repetitive work?
+'How often do you have difficulty keeping your attention when you are doing boring or repetitive work?',
 
-How often do you have difficulty
-concentrating on what people say to you,
-even when they are speaking to you directly?
+'How often do you have difficulty concentrating on what people say to you, even when they are speaking to you directly?',
 
-How often do you misplace or have
-difficulty finding things at home or at
-work?
+'How often do you misplace or have difficulty finding things at home or at work?',
 
-How often are you distracted by
-activity or noise around you? 
+'How often are you distracted by activity or noise around you?',
 
-How often do you leave your seat in
-meetings or other situations in which you
-are expected to remain seated?
+'How often do you leave your seat in meetings or other situations in which you are expected to remain seated?',
 
-How often do you feel restless or
-fidgety? 
+'How often do you feel restless or fidgety?',
 
-How often do you have difficulty
-unwinding and relaxing when you have
-time to yourself?
+'How often do you have difficulty unwinding and relaxing when you have time to yourself?',
 
-How often do you find yourself talking
-too much when you are in social
-situations?
+'How often do you find yourself talking too much when you are in social situations?',
 
-When you’re in a conversation, how often do you
-find yourself finishing the sentences of the people
-you are talking to, before they can finish them
-themselves?
+'When you’re in a conversation, how often do you find yourself finishing the sentences of the people you are talking to, before they can finish them themselves?',
 
-How often do you have difficulty waiting
-your turn in situations when turn taking is
-required?
+'How often do you have difficulty waiting your turn in situations when turn taking is required?',
 
-How often do you interrupt others
-when they are busy? '''
+'How often do you interrupt others when they are busy?']
+
+
 st.text(options)
-st.text(text)
+st.text([question for question in text])
 #text = [part for part in text.split('\n') if part.strip() not in ['Page 1 of 2', 'NovoPsych','PART A -','PART B -']]
 
 # for line in text:
